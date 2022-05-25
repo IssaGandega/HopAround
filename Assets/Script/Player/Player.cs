887fa1e753de120acbf56a2ac93a65942563882c
Mynns <60692575+Mynns@users.noreply.github.com>
2022-05-25 16:38:35 +0200

    merge avec juju hihihihi

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -116,7 +116,8 @@ public class Player : MonoBehaviour
 
         if (!isJumping)
         {
-            isGrounded = Physics2D.OverlapCircle(groundedCheckerPos.position, 0.3f, groundLayer);
+            isGrounded = Physics2D.OverlapCircle(groundedCheckerPos.position, 0.3f, groundLayer) ||
+                Physics2D.OverlapCircle(groundedCheckerPos.position, 0.3f, tongueLayer);
         }
     }
 
@@ -128,7 +129,7 @@ public class Player : MonoBehaviour
             coyoteTimeCounter = coyoteTime;
             
             //Not moving if not tilted enough / Touched
-            if (xAxisAccel > -0.2f && xAxisAccel < 0.2f)
+            if (xAxisAccel > -0.2f*rotateMaxValue && xAxisAccel < 0.2f * rotateMaxValue)
             {
                 rb.velocity = Vector3.zero;
                 currentSpeed = 0;
@@ -163,7 +164,7 @@ public class Player : MonoBehaviour
             }
             
             //Reset speed and turn
-            if ((isFacingRight && xAxisAccel < -0.1 || !isFacingRight && xAxisAccel > 0.1))
+            if ((isFacingRight && xAxisAccel < -0.1 * rotateMaxValue || !isFacingRight && xAxisAccel > 0.1 * rotateMaxValue))
             {
                 if (isGrounded)
                 {
